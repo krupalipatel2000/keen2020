2763681269bad74b9f9d2feb780cd3bce3644655
Nandani Borse <nandaniborse@gmail.com>
2020-02-27 17:28:10 +0530

    client

---
+++ KeenConveyance/Controllers/ClientUserController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KeenConveyance.Areas.Admin.Models;
+
+namespace KeenConveyance.Controllers
+{
+    public class ClientUserController : Controller
+    {
+        // GET: User
+        dbTransportEntities3 dc = new dbTransportEntities3();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(FormCollection form)
+        {
+            tblUser user = new tblUser();
+            user.ProfilePic = form["txtprofile"];
+            user.FirstName = form["txtFirstName"];
+            user.LastName = form["txtLastName"];
+            user.EmailId = form["txtEmailId"];
+            user.Password = form["txtPwd"];
+            user.Gender = form["txtGender"];
+            user.ContactNo = form["txtCno"];
+            user.IsActive = false;
+            user.CreatedOn = DateTime.Now;
+            user.IsVerified = false;
+            user.IsMobileVerified = false;
+            dc.tblUsers.Add(user);
+            dc.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            var user = dc.tblUsers.ToList();
+            return View(user);
+        }
+        [HttpPost]
+        public JsonResult Active(int id)
+        {
+            tblUser user = dc.tblUsers.SingleOrDefault(ob => ob.UserId == id);
+            if (user.IsActive == true)
+            {
+                user.IsActive = false;
+            }
+            else
+            {
+                user.IsActive = true;
+            }
+            dc.SaveChanges();
+            return Json(user.IsActive, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+}
