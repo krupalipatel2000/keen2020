cf1fd92bae7efc4d446cfb2d2b89b27434be01a1
krupali patel <krupalipatel036@gmail.com>
2020-04-14 12:09:58 +0530

    contactus

---
+++ KeenConveyance/Areas/Admin/Models/Googlemap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KeenConveyance.Areas.Admin.Models
+{
+    public class Googlemap
+    {
+        public string Title { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Lng { get; set; }
+
+    }
+}
--- KeenConveyance/Controllers/ClientContactController.cs
+++ KeenConveyance/Controllers/ClientContactController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Configuration;
+using KeenConveyance.Areas.Admin.Models;
 
 namespace KeenConveyance.Controllers
 {
@@ -11,8 +11,24 @@ namespace KeenConveyance.Controllers
     {
         // GET: ClientContact
         public ActionResult Index()
-        {   
-            return View();
+        {
+            var cities = new List<Googlemap>();
+            cities.Add(new Googlemap() { Title = "Paris", Lat = 48.855901, Lng = 2.349272 });
+            cities.Add(new Googlemap() { Title = "Berlin", Lat = 52.520413, Lng = 13.402794 });
+            cities.Add(new Googlemap() { Title = "Rome", Lat = 41.907074, Lng = 12.498474 });
+            return View(cities);
         }
+        [HttpPost]
+        public JsonResult GetAnswer(string question)
+        {
+            int index = _rnd.Next(_db.Count);
+            var answer = _db[index];
+            return Json(answer);
+        }
+
+        private static Random _rnd = new Random();
+
+        private static List<string> _db = new List<string> { "Yes", "No", "Definitely, yes", "I don't know", "Looks like, yes" };
     }
+}
 }
