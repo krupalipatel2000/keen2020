20c04b8a5cc7bc2a34ef77e0a1949f20d7922e93
krupali patel <krupalipatel036@gmail.com>
2020-05-07 14:12:11 +0530

    5/7/2020

--- KeenConveyance/Controllers/ClientBookingController.cs
+++ KeenConveyance/Controllers/ClientBookingController.cs
@@ -11,9 +11,23 @@ namespace KeenConveyance.Controllers
     {
         // GET: ClientBooking
         dbTransportEntities5 dc = new dbTransportEntities5();
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
-            return View();
+            var Booking = from ob in dc.tblBookings
+                          join ob2 in dc.tblConsignments on ob.ConsignmentId equals ob2.ConsignmentId
+                          join ob3 in dc.tblUsers on ob2.UserId equals ob3.UserId
+                          join ob4 in dc.tblDrivers on ob.DriverId equals ob4.DriverId
+                          join ob5 in dc.tblVehicles on ob.VehicleId equals ob5.VehicleId
+                          select new JoinViewAll
+                          {
+                              user = ob3,
+                              consignment = ob2,
+                              book = ob,
+                              driver = ob4,
+                              vehicle = ob5
+                          };
+            ViewBag.Booking = Booking;
+            return View(Booking);
         }
         public ActionResult Insert()
         {
